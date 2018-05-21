f96a1e90a752a0df84479ed6c05163412b098ab1
Tristan <tkidder132@gmail.com>
2018-05-20 20:31:12 -0500

    Added stone and grass layers

--- Assets/_Scripts/World.cs
+++ Assets/_Scripts/World.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
+
     public Material textureAtlas;
     public static int columnHeight = 16;
     public static int chunkSize = 16;
-    public int worldSize = 2;
+    public static int worldSize = 2;
     public static Dictionary<string, Chunk> chunks;
 
     public static string BuildChunkName(Vector3 v)
