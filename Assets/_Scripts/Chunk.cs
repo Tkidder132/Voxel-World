3d0a204d235642572382dda0c045c7b08d7d55ae
Tristan <tkidder132@gmail.com>
2018-06-03 04:00:15 -0500

    Add more block manipulation, including building blocks. updated block atlas

--- Assets/_Scripts/Chunk.cs
+++ Assets/_Scripts/Chunk.cs
@@ -24,16 +24,19 @@ class BlockData
     }
 }
 
-
 public class Chunk
 {
 
     public Material cubeMaterial;
+    public Material fluidMaterial;
     public Block[,,] chunkData;
     public GameObject chunk;
+    public GameObject fluid;
     public enum ChunkStatus { DRAW, DONE, KEEP };
     public ChunkStatus status;
+    public ChunkMB mb;
     BlockData bd;
+    public bool changed = false;
 
     string BuildChunkFileName(Vector3 v)
     {
@@ -48,34 +51,33 @@ public class Chunk
 
     bool Load() //read data from file
     {
-        string chunkFile = BuildChunkFileName(chunk.transform.position);
-        if (File.Exists(chunkFile))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(chunkFile, FileMode.Open);
-            bd = new BlockData();
-            bd = (BlockData)bf.Deserialize(file);
-            file.Close();
-            //Debug.Log("Loading chunk from file: " + chunkFile);
-            return true;
-        }
+        /*string chunkFile = BuildChunkFileName(chunk.transform.position);
+		if(File.Exists(chunkFile))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(chunkFile, FileMode.Open);
+			bd = new BlockData();
+			bd = (BlockData) bf.Deserialize(file);
+			file.Close();
+			//Debug.Log("Loading chunk from file: " + chunkFile);
+			return true;
+		}*/
         return false;
     }
 
     public void Save() //write data to file
     {
-        string chunkFile = BuildChunkFileName(chunk.transform.position);
-
-        if (!File.Exists(chunkFile))
-        {
-            Directory.CreateDirectory(Path.GetDirectoryName(chunkFile));
-        }
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Open(chunkFile, FileMode.OpenOrCreate);
-        bd = new BlockData(chunkData);
-        bf.Serialize(file, bd);
-        file.Close();
-        //Debug.Log("Saving chunk from file: " + chunkFile);
+        /*string chunkFile = BuildChunkFileName(chunk.transform.position);
+		
+		if(!File.Exists(chunkFile))
+		{
+			Directory.CreateDirectory(Path.GetDirectoryName(chunkFile));
+		}
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Open(chunkFile, FileMode.OpenOrCreate);
+		bd = new BlockData(chunkData);
+		bf.Serialize(file, bd);
+		file.Close();*/
     }
 
     void BuildChunk()
@@ -141,6 +143,17 @@ public class Chunk
 
     }
 
+    public void Redraw()
+    {
+        GameObject.DestroyImmediate(chunk.GetComponent<MeshFilter>());
+        GameObject.DestroyImmediate(chunk.GetComponent<MeshRenderer>());
+        GameObject.DestroyImmediate(chunk.GetComponent<Collider>());
+        GameObject.DestroyImmediate(fluid.GetComponent<MeshFilter>());
+        GameObject.DestroyImmediate(fluid.GetComponent<MeshRenderer>());
+        GameObject.DestroyImmediate(fluid.GetComponent<Collider>());
+        DrawChunk();
+    }
+
     public void DrawChunk()
     {
         for (int z = 0; z < World.chunkSize; z++)
@@ -149,28 +162,36 @@ public class Chunk
                 {
                     chunkData[x, y, z].Draw();
                 }
-        CombineQuads();
+        CombineQuads(chunk.gameObject, cubeMaterial);
         MeshCollider collider = chunk.gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
         collider.sharedMesh = chunk.transform.GetComponent<MeshFilter>().mesh;
+
+        CombineQuads(fluid.gameObject, fluidMaterial);
         status = ChunkStatus.DONE;
     }
 
     public Chunk() { }
     // Use this for initialization
-    public Chunk(Vector3 position, Material c)
+    public Chunk(Vector3 position, Material c, Material t)
     {
 
         chunk = new GameObject(World.BuildChunkName(position));
         chunk.transform.position = position;
+        fluid = new GameObject(World.BuildChunkName(position) + "_F");
+        fluid.transform.position = position;
+
+        mb = chunk.AddComponent<ChunkMB>();
+        mb.SetOwner(this);
         cubeMaterial = c;
+        fluidMaterial = t;
         BuildChunk();
     }
 
 
-    public void CombineQuads()
+    public void CombineQuads(GameObject o, Material m)
     {
         //1. Combine all children meshes
-        MeshFilter[] meshFilters = chunk.GetComponentsInChildren<MeshFilter>();
+        MeshFilter[] meshFilters = o.GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
         int i = 0;
         while (i < meshFilters.Length)
@@ -181,18 +202,18 @@ public class Chunk
         }
 
         //2. Create a new mesh on the parent object
-        MeshFilter mf = (MeshFilter)chunk.gameObject.AddComponent(typeof(MeshFilter));
+        MeshFilter mf = (MeshFilter)o.gameObject.AddComponent(typeof(MeshFilter));
         mf.mesh = new Mesh();
 
         //3. Add combined meshes on children as the parent's mesh
         mf.mesh.CombineMeshes(combine);
 
         //4. Create a renderer for the parent
-        MeshRenderer renderer = chunk.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
-        renderer.material = cubeMaterial;
+        MeshRenderer renderer = o.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+        renderer.material = m;
 
         //5. Delete all uncombined children
-        foreach (Transform quad in chunk.transform)
+        foreach (Transform quad in o.transform)
         {
             GameObject.Destroy(quad.gameObject);
         }
